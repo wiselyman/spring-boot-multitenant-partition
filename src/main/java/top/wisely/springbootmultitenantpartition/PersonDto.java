2b416a007c8c5310efa114bc09cdfe3677b42589
Yunfei Wang <wiselyman2008@gmail.com>
2022-10-12 22:35:59 +0800

    add some more features

--- src/main/java/top/wisely/springbootmultitenantpartition/PersonDto.java
+++ src/main/java/top/wisely/springbootmultitenantpartition/PersonDto.java
@@ -3,11 +3,9 @@ package top.wisely.springbootmultitenantpartition;
 import lombok.Value;
 
 @Value
-
 public class PersonDto {
     private String name;
     private Integer age;
-
     public Person createPerson(){
         Person person = new Person();
         person.setName(this.name);
